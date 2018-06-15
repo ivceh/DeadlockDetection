b5ee793578899165f41dc3675a60a69ec57523ac
ivceh <ivan.ceh1234@gmail.com>
2018-06-15 19:16:13 +0200

    Initial commit

---
+++ DeadlockDetection/src/AndCMH.java
@@ -0,0 +1,73 @@
+import java.util.stream.IntStream;
+
+public class AndCMH extends Process {
+	boolean dependent[];
+	int requests[], replies[];
+	boolean blocked = false;
+	
+	public AndCMH(Linker initComm, int initialRequests[]) {
+		super(initComm);
+		dependent = new boolean[N]; // all false by default
+		requests = new int[N]; // all 0 by default
+		replies = new int[N]; // all 0 by default
+		Initialize(initialRequests);
+	}
+	
+	public void Initialize(int initialRequests[]) {
+		for(int i=0; i<initialRequests.length; ++i) {
+			sendMsg(initialRequests[i], "REQUEST", "");
+			Util.mySleep(1000);
+		}
+	}
+	
+	public void sendMsg(int destId, String tag, String msg) {
+        super.sendMsg(destId, tag, msg);
+        if (tag == "REQUEST")
+        	++requests[destId];
+        else if (tag == "REPLY")
+        	--replies[destId];
+    }
+	
+	public synchronized void handleMsg(Msg m, int src, String tag) {
+		if (tag == "REQUEST") {
+			sendMsg(src, "REPLY", "");
+			++replies[src];
+		}
+		else if (tag == "REPLY") {
+			--requests[src];
+			//if (IntStream.of(requests).allMatch(x -> (x==0)))
+			blocked = false;
+			for (int i=0; i<N; ++i)
+				if (requests[i] != 0) {
+					blocked = true;
+					break;
+				}
+			
+			if (blocked) {
+				if (dependent[myId])
+					DeclareDeadlock();
+				else {
+					for (int j=0; j<N; ++j)
+						if (requests[j] > 0)
+							sendMsg(j, "PROBE", myId);
+				}
+			}
+		}
+		else if (tag == "PROBE") {
+			if (blocked && !dependent[m.getMessageInt()] && replies[src]>0) {
+				dependent[m.getMessageInt()] = true;
+				if (myId == m.getMessageInt())
+					DeclareDeadlock();
+				else {
+					for (int j=0; j<N; ++j)
+						if (requests[j] > 0)
+							sendMsg(j, "PROBE", m.getMessageInt());
+				}
+			}
+		}
+    }
+	
+	private void DeclareDeadlock() {
+		Util.println("DEADLOCK!!!");
+	}
+}
