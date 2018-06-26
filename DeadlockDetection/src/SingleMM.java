eeb6f203d53708b4f5236f58e7979a9a079e67e9
ivceh <ivan.ceh1234@gmail.com>
2018-06-26 23:54:51 +0200

    SinglMM works

--- DeadlockDetection/src/SingleMM.java
+++ DeadlockDetection/src/SingleMM.java
@@ -1,6 +1,10 @@
 import java.util.stream.IntStream;
+import java.util.HashSet;
 
 public class SingleMM extends Process {
+	
+	int publicLabel = myId, privateLabel = myId;
+	HashSet<Integer> waitingForMe = new HashSet<Integer>();
 	
 	public SingleMM(Linker initComm, int initialRequests[]) {
 		super(initComm);
@@ -15,10 +19,41 @@ public class SingleMM extends Process {
 	}
 	
 	public synchronized void handleMsg(Msg m, int src, String tag) {
-		
+		if (tag.equals("REQUEST")) {
+			sendMsg(src, "BLOCK", publicLabel);
+			waitingForMe.add(src);
+		}
+		else if (tag.equals("BLOCK")) {
+			publicLabel = privateLabel = inc(publicLabel, m.getMessageInt());
+			Transmit();
+		}
+		else if (tag.equals("ACTIVATE")) {
+			waitingForMe.remove(src);
+		}
+		else if (tag.equals("TRANSMIT")) {
+			if (m.getMessageInt() > publicLabel) {
+				publicLabel = m.getMessageInt();
+				Transmit();
+			}
+			else if (m.getMessageInt() == publicLabel && publicLabel == privateLabel)
+				DeclareDeadlock();
+		}
+		else
+			throw new IllegalArgumentException("Unknown message tag \"" + tag + "\"!");
     }
 	
 	private void DeclareDeadlock() {
 		Util.println("DEADLOCK!!!");
+	}
+	
+	private int inc(int x, int y) {
+		// uniqueness is assured by holding  inc ≡ myId (mod N)
+		int r = Util.max(x, y);
+		return ((r - myId) / N + 1) * N + myId;
+	}
+	
+	private void Transmit() {
+		for (int proc : waitingForMe)
+			sendMsg(proc, "TRANSMIT", publicLabel);
 	}
 }
